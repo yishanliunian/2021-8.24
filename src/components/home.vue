3e699e554300737eeba45103b9d8651c27a0b545
heyuhang <1337535208@.qq.com>
2021-08-24 07:57:53 +0800

    装修预约

--- src/components/home.vue
+++ src/components/home.vue
@@ -47,78 +47,95 @@
            <i class="iconfont icon-price"><span>装修贷款</span></i>
          </p>
       </div>
+      <!-- 装修预定 -->
+      <div class="zhuang">
+          <p class="shang"><span class="xiu">装修预订</span>
+          <span class="bao">免费上门量房，设计方案满意为止</span>
+        </p>
+        <p class="inp">
+          <input type="text" placeholder="小区名">
+        </p>
+        <p class="inp">
+          <input type="text" placeholder="手机号">
+        </p>
+        <p class="inp">
+          <input type="text" placeholder="建筑面积">
+        </p>
+        <p class="inp">
+          <button class="btn">立即申请</button>
+        </p>
+      </div>
   </div>
 </template>
 <style lang="scss">
-*{
+* {
   margin: 0;
   padding: 0;
 }
-#app{
+#app {
   width: 100%;
-  height:800px;
+  height: 800px;
   background: #cccccc;
 }
-.ding{ 
+.ding {
   width: 100%;
   height: 60px;
   background: white;
-  
-  .fang{
+
+  .fang {
     width: 65px;
     height: 30px;
     margin-left: 18px;
     margin-top: 16px;
     margin-bottom: 16px;
   }
-  .tu{
-      position: fixed;
-      top: 16px;
-      right: 6px;
-      text-align: center;
-      
-     .wo{
-    width: 40px;
-    height: 35px;
-    font-size: 10px;
-    color: #999999;
-    float: left;
-    img{
-      width: 20px;
-      height: 25px;
+  .tu {
+    position: fixed;
+    top: 16px;
+    right: 6px;
+    text-align: center;
+
+    .wo {
+      width: 40px;
+      height: 35px;
+      font-size: 10px;
+      color: #999999;
+      float: left;
+      img {
+        width: 20px;
+        height: 25px;
+      }
+    }
+    .dao {
+      width: 40px;
+      height: 35px;
+      font-size: 10px;
+      color: #999999;
+      margin-left: 20px;
+      float: left;
+      .icon-user {
+        color: #999999;
+        width: 20px;
+        height: 25px;
+      }
     }
   }
-  .dao{
-    width: 40px;
-    height: 35px;
-    font-size: 10px;
-    color: #999999;
-   margin-left: 20px;
-   float: left;
-   .icon-user{
-     color: #999999;
-       width: 20px;
-      height: 25px;
-    }
-  }
-  }
- 
 }
-.lun{
+.lun {
   width: 100%;
-  height:150px;
-  img{
-     width: 360px;
   height: 150px;
+  img {
+    width: 360px;
+    height: 150px;
   }
 }
-.biao{
+.biao {
   width: 100%;
   height: 220px;
   background: white;
-  ul{
+  ul {
     list-style: none;
-    li{
+    li {
       width: 50px;
       height: 50px;
       border-radius: 50%;
@@ -127,80 +144,115 @@
       margin-left: 30px;
       margin-top: 35px;
       text-align: center;
-      p{
-        
+      p {
         font-size: 10px;
       }
-      i{
-      font-size: 29px;
-      color:white;
-      padding-top: 60px;
-      line-height: 65px;
+      i {
+        font-size: 29px;
+        color: white;
+        padding-top: 60px;
+        line-height: 65px;
+      }
     }
-      
+
+    .li1 {
+      background: #39c894;
     }
-    
-    .li1{
-      background:#39c894;
+    .li2 {
+      background: #feb505;
     }
-    .li2{
-      background:#feb505;
+    .li3 {
+      background: #eed215;
     }
-    .li3{
-      background:#eed215;
+    .li4 {
+      background: #00b2b2;
     }
-    .li4{
-      background:#00b2b2;
+    .li5 {
+      background: #1b9af7;
     }
-    .li5{
-      background:#1b9af7;
+    .li6 {
+      background: #7b72e9;
     }
-    .li6{
-      background:#7b72e9;
+    .li7 {
+      background: #ff4351;
     }
-    .li7{
-      background:#ff4351;
-    }
-    .li8{
-      background:#fe8864;
+    .li8 {
+      background: #fe8864;
     }
   }
 }
-.quan{
+.quan {
   width: 100%;
   height: 300px;
   background: white;
-  .shang{
+  .shang {
     width: 100%;
     height: 30px;
-    margin-top: 30px;
-    text-align: center;
-    .xiu{
+    margin-top: 10px;
+
+    .xiu {
       font-size: 20px;
       color: black;
-      margin-left: 15px;
+      margin-left: 10px;
     }
-    .bao{
+    .bao {
       font-size: 15px;
       color: #999999;
       margin-left: 15px;
     }
   }
-  .xia{
+  .xia {
     margin-top: 36px;
     width: 100%;
     font-size: 15px;
-    i{
+    i {
       color: #39c894;
-      span{
+      span {
         color: black;
       }
     }
   }
-  .lun2{
+  .lun2 {
     width: 100%;
     height: 150px;
     margin-top: 30px;
+  }
+}
+.zhuang {
+  width: 100%;
+  height: 300px;
+  background: white;
+  text-align: center;
+  .shang {
+    width: 100%;
+    height: 30px;
+    margin-top: 10px;
+    .xiu {
+      font-size: 20px;
+      color: black;
+      margin-left: 10px;
+    }
+    .bao {
+      font-size: 15px;
+      color: #999999;
+      margin-left: 15px;
+    }
+  }
+  .inp{
+    margin-top: 25px;
+    input{
+      width: 300px;
+      height: 31px;
+      border: 1px solid #999999;
+      color: #999999;
+    }
+    .btn{
+      width: 300px;
+      height: 31px;
+      background: #feb505;
+      color: white;
+      border: 1px solid white;
+    }
   }
 }
 </style>
